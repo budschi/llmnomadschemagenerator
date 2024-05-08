89b68d1524deec82cc5e3d0783a93ab51d0374a2
Rubel <rubel.mozumder@outlook.com>
2024-05-08 13:49:49 +0200

    reading from pdf.

--- llmschemagen.py
+++ llmschemagen.py
@@ -6,6 +6,7 @@ from nomad.client import parse, normalize_all
 import yaml
 import ast
 import streamlit as st
+import os
 import traceback
 
 llm = Ollama(model="llama3:70b")
@@ -140,24 +141,90 @@ chain = prompt | llm | output_parser
 
 
 def invokechain(userinput):
-    #userinput = input("Please enter your input: ")
+  """Input text from user
 
-    return chain.invoke({"userpromp": userinput }) 
-# Title of the app
-st.title('NOMAD Schema Generator App')
-appdescription="""
-This Generator App can create Custom yaml schemas for NOMAD (www.nomad-lab.eu).
-It takes the user prompt, generates a yaml schema and prints it below if it 
-was verified successfully to be a working NOMAD schema. 
-In case the LLM generates errors it will try to fix the schema and try again 
-up to 5 times. If it still fails, it will print the error message."""
-st.text(appdescription)
-# Text input field
-# user_input = st.text_input("Please enter your input:")
-user_input = st.text_area("Enter your description of your experiment here:")
+  Parameters
+  ----------
+  userinput : str
+  """
+  return chain.invoke({"userpromp": userinput })
 
-# Button
-if st.button('Generate Schema'):
+def convert_pdf_to_text(pdf_file):
+   # Read the pdf
+
+    pdf_file = "/home/rubel/Downloads/delete_2/papers/152101_1_online.pdf"
+
+
+    from langchain_community.document_loaders import PyPDFLoader
+
+    loader = PyPDFLoader(pdf_file)
+    pages = loader.load_and_split()
+
+    text = ""
+    for page in pages:
+        text += page.page_content
+
+    text = text.encode('utf-8')
+    return extract_experiment_info_from_text(text)
+
+def extract_experiment_info_from_text(text):
+  """
+  Extract text from text file
+  """
+  cheatsheet=    """
+You are a physical quantity extractor. Below you will get some data from a lab. You should output the lab and experiment information and no other text!
+here is a snippet from cheat sheet to write information about a lab and experiment from a lab sheet:
+# Description: This is a cheat sheet for experimental lab and user description.
+#   It is a minimal example of how to a experiment and lab.
+#   It is intended to be used as a starting point for definig a experiment and lab.
+
+MyLabAndExperimentInfo:
+  temperature: 394.5K
+  process_finished: True
+  user: Sebastian
+"""
+
+  prompt = ChatPromptTemplate.from_template(cheatsheet + "{userpromp}")
+  output_parser = StrOutputParser()
+  chain = prompt | llm | output_parser
+
+  result = chain.invoke({"userpromp": text })
+  return result
+   
+def get_user_input():
+    counter = 0
+    while counter < 5:
+        user_input = st.text_area("Enter your description of your experiment here:")
+
+        file = st.file_uploader("Please enter a file e.g. pdf, text")
+        try:
+            if os.path.isfile(file):
+                if isinstance(user_input, str):
+                    st.text("Please enter only one input either file or text.")
+                    counter += 1
+                    continue
+                break
+        except Exception as e:
+            if isinstance(user_input, str):
+                break
+            counter += 1
+            pass
+                       
+    try:
+      if os.path.isfile(file):
+        if file.endswith('.pdf'):
+          user_input = convert_pdf_to_text(file)
+          st.text(user_input)
+          return user_input
+    except Exception as e:
+      if isinstance(user_input, str):
+        return user_input
+    else:
+        raise ValueError("Please enter a valid input")
+
+
+def run_llm(user_input, max_iter):
+   if st.button('Generate Schema'):
     # Call the invokechain function with the user's input
     result = invokechain(user_input)
     #result = yaml.safe_load(ast.literal_eval(result))
@@ -169,17 +236,18 @@ if st.button('Generate Schema'):
     #except Exception:
     #    error_message = traceback.format_exc()
     #    st.write(error_message)    
-    counter=1
+    counter=0
     error = None
     try:
         # Replace this with the actual parse command
         parse('output.schema.archive.yaml')
     except Exception as e:
         error = e
+        st.write(counter)
         st.write(f"An error occurred: {error}")
     
-        for counter in range(5):
-            #counter += 1
+        for counter in range(max_iter):
+            counter += 1
             st.write(counter)
             result = invokechain("You produced the following schema:" + result + "\n It returned this error: " + str(error) + "Please fix the schema and try again.")
             #result = yaml.safe_load(ast.literal_eval(result))
@@ -200,18 +268,37 @@ if st.button('Generate Schema'):
                 error = e
                 st.write(f"An error occurred: {error}")
         # Display the result
+    st.markdown(f"```attempt result```: {counter}")
 
     st.text("``` \n" + result)
-    #else:
-    # Download button
-with open('output.schema.archive.yaml', 'r') as file:
-    file_content = file.read()
-st.download_button(
-    label="Download output.schema.archive.yaml",
-    data=file_content,
-    file_name="output.schema.archive.yaml",
-    mime="application/x-yaml"
+
+def download_schema():
+  with open('output.schema.archive.yaml', 'r') as file:
+      file_content = file.read()
+  st.download_button(
+      label="Download output.schema.archive.yaml",
+      data=file_content,
+      file_name="output.schema.archive.yaml",
+      mime="application/x-yaml"
 )
+
+def init_app():
+    # Title of the app
+    st.title('NOMAD Schema Generator App')
+    appdescription="""
+    This Generator App can create Custom yaml schemas for NOMAD (www.nomad-lab.eu).
+    It takes the user prompt, generates a yaml schema and prints it below if it 
+    was verified successfully to be a working NOMAD schema. 
+    In case the LLM generates errors it will try to fix the schema and try again 
+    up to 5 times. If it still fails, it will print the error message."""
+    st.text(appdescription)
+    user_input = get_user_input()
+    run_llm(user_input, 5)
+    download_schema()
+
+
+
+init_app()
         
 text = """
 Example input:
